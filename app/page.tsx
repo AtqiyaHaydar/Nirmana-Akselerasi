82d3b769fb448e2dd736207d64f4bc0f9cdd278f
AtqiyaHaydar <ashford1524@gmail.com>
2024-03-03 19:14:55 +0700

    feat: add decoration

--- app/page.tsx
+++ app/page.tsx
@@ -22,11 +22,22 @@ export default function Home() {
       <Kenneth />
         
       {/* Quote */}
-      <p className="px-4 max-w-4xl text-white text-center font-source-code-pro py-20">
+      <p className="px-4 max-w-4xl text-white text-center font-source-code-pro pt-20">
         "HMIF JADI WADAH YANG SESUAI, YANG BISA MEMENUHI KEBUTUHAN
         MAHASISWANYA DAN BISA BERDAMPAK BAGI MASYARAKAT!" <br/> <br/> - Kenneth
       </p>
 
+      {/* Meteor */}
+      <video
+        autoPlay
+        muted
+        loop
+        className="h-[75px] w-[75px] md:h-[150px] md:w-[150px] object cover"
+      >
+        <source src="/11.webm" type="video/webm" />
+      </video>
+
+      <div className="pb-20" />
       <VisiMisi />
 
       {/* Question */}
@@ -40,11 +51,22 @@ export default function Home() {
       <p className="px-4 text-white text-center font-source-code-pro pt-20 pb-4">
         Lihat Selengkapnya Di 
       </p>
-      <div className="text-space font-source-code-pro bg-primaryOrange rounded-xl px-16 py-4 cursor-pointer z-30 hover:bg-primaryOrange/75">
+      <div className="text-space font-source-code-pro bg-primaryOrange rounded-xl px-16 py-4 mb-8 cursor-pointer z-30 hover:bg-primaryOrange/75">
         <a target="_blank" href="https://drive.google.com/file/d/12UJR-MGnlqnHfBDVZ9oJTPR1malDLVBv/view">
           Draft Kenneth
         </a>
       </div>
+
+      {/* Astronomy */}
+      {/* Meteor */}
+      <video
+        autoPlay
+        muted
+        loop
+        className=" h-[150px] w-[150px] md:h-[300px] md:w-[300px] object cover"
+      >
+        <source src="/9.webm" type="video/webm" />
+      </video>
   
     </main>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,15 @@ import Ken from '@/public/Ken.png'
 const Header = () => {
   return (
     <header className='w-full flex flex-col items-center justify-center '>
+      {/* Roket */}
+      <video
+        autoPlay
+        muted
+        loop
+        className="h-[75px] w-[75px] md:h-[125px] md:w-[125px] object-cover absolute right-4 lg:right-[200px] top-8 lg:top-4"
+      >
+        <source src="/10.webm" type="video/webm" />
+      </video>
       <div className='space-y-2 lg:space-y-4 text-center text-white'>
         <h1 className='font-source-code-pro text-3xl md:text-5xl lg:text-7xl'>
           Nirmana Akselerasi
